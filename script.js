26d11498ab4f115ce4990c19b2a25a648a8f5110
Skylark355 <adzesi.chris@gmail.com>
2024-11-26 10:54:00 +0000

    Working o a tip calculator

---
+++ script.js
@@ -0,0 +1,8 @@
+const billAmount = document.getElementById("billAmount");
+const customTipAmount = document.getElementById("customTipAmount");
+const splitCount = document.getElementById("splitCount");
+
+// const submitBtn = document.getElementById("")
+const totalAmount = document.getElementById("totalAmount");
+const tipAmount = document.getElementById("tipAmount");
+const amountPerHead = document.getElementById("amountPerPerson");
