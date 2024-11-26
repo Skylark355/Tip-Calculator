f360175a021361c07082996265187a62f316758d
Skylark355 <adzesi.chris@gmail.com>
2024-11-26 14:29:53 +0000

    Tip Calculator

--- script.js
+++ script.js
@@ -6,3 +6,47 @@ const splitCount = document.getElementById("splitCount");
 const totalAmount = document.getElementById("totalAmount");
 const tipAmount = document.getElementById("tipAmount");
 const amountPerHead = document.getElementById("amountPerPerson");
+
+function submit() {
+  const bill = parseFloat(billAmount.value);
+  const tip = parseFloat(customTipAmount.value);
+  const numberOfPeople = splitCount.value;
+  if (!bill || !tip || !numberOfPeople) {
+    alert(`Some fields are empty`);
+  } else {
+    const totalBill = parseFloat(bill + tip);
+    const amountPerPerson = parseFloat(totalBill / numberOfPeople);
+
+    totalAmount.innerHTML = `The total amount is: ${totalBill}`;
+    tipAmount.innerHTML = `Your tip is ${tip}`;
+    amountPerHead.innerHTML = `Each person pays: ${amountPerPerson.toFixed(2)}`;
+  }
+
+  //   reset();
+}
+
+function reset() {
+  billAmount.value = "";
+  customTipAmount.value = "";
+  splitCount.value = "";
+
+  totalAmount.innerHTML = "";
+  tipAmount.innerHTML = "";
+  amountPerPerson.innerHTML = "";
+
+  //   document.getElementById("billAmount").value = "";
+  //   document.getElementById("customTipAmount").value = "";
+  //   document.getElementById("splitCount").value = "";
+}
+
+function toggleDivVisibility() {
+  const serviceRatingRadioButton = document.getElementById("serviceRating");
+  const serviceRatingInputDiv = document.getElementById("serviceRatingInput");
+
+  if (serviceRatingRadioButton.checked) {
+    serviceRatingInputDiv.style.display = "block";
+  } else {
+    serviceRatingInputDiv.style.display = "none";
+  }
+}
+
